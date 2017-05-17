4805a4de0d3f4662322e6773778e777d687c1d6a
zhpebox <zhpebox@163.com>
2017-05-17 14:33:22 +0800

    修改一个jar包

--- src/main/java/com/book/controller/UserController.java
+++ src/main/java/com/book/controller/UserController.java
@@ -3,8 +3,6 @@ package com.book.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
