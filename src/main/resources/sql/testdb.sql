95f059e4cabdf96d05a5dafc7c1e03b8a72864d2
zhpebox <zhpebox@163.com>
2017-05-18 13:53:44 +0800

    加入数据库脚本

---
+++ src/main/resources/sql/testdb.sql
@@ -0,0 +1,48 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50148
+Source Host           : localhost:3306
+Source Database       : testdb
+
+Target Server Type    : MYSQL
+Target Server Version : 50148
+File Encoding         : 65001
+
+Date: 2017-05-16 17:04:36
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tbl_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE `tbl_user` (
+  `uid` int(3) NOT NULL COMMENT '自增ID',
+  `uname` varchar(50) DEFAULT NULL COMMENT '用户名',
+  `upass` varchar(50) DEFAULT NULL COMMENT '用户密码',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_user
+-- ----------------------------
+INSERT INTO `tbl_user` VALUES ('3', 'wang', 'asd');
+
+-- ----------------------------
+-- Table structure for test
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `Id` int(11) NOT NULL DEFAULT '0',
+  `test` blob,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
+INSERT INTO `test` VALUES ('1', 0x08890612057368697169);
+INSERT INTO `test` VALUES ('3', 0x08890612057368697169);
